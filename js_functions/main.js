9d1d906e14ce87dba482bd7a0965a7d13db28cf1
stayasleep <virtuallyinfekted@gmail.com>
2017-02-22 14:44:36 -0800

    js functions - brian bernstein

---
+++ js_functions/main.js
@@ -0,0 +1,16 @@
+function myMessage(){
+    console.log("Have a Great Day!");
+}
+function add(x,y){
+    var result = x+y;
+    //console.log(result);
+}
+function add2(x,y){
+    var result =x+y;
+    return result;
+}
+var add2result = add2(11,14);
+function cardFlip(element){
+    $(element).hide();
+}
+
