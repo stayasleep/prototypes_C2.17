7f5cff0130bee2d7d619fc2c74c7f2055afccd2c
stayasleep <virtuallyinfekted@gmail.com>
2017-03-30 12:39:58 -0700

    PHP Super Globals - Brian Bernstein

---
+++ php_superglobals/index.php
@@ -0,0 +1,9 @@
+<?php
+    print_r($_GET);
+//    print_r($_GET['_ijt']. ", I have a message for you: ");
+    var_dump($_POST);
+//    $serverStr=$_SERVER["HTTP_USER_AGENT"];
+//    if (strpos($serverStr,"Chromen")){
+//        print("you are using the Chrome browser");
+//    };
+?>
